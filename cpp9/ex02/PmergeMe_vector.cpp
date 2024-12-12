ea9da879ee3b913faf88a9131887c4a992a1b8ab
mschulme <marcschulmeister@hotmail.de>
2024-12-12 23:22:08 +0100

    CPP09

--- cpp9/ex02/PmergeMe_vector.cpp
+++ cpp9/ex02/PmergeMe_vector.cpp
@@ -1,6 +1,9 @@
 #include "PmergeMe.hpp"
 
 
+static const int GROUP_SIZES[] = {2, 2, 6, 10, 22, 42, 86, 170, 342, 682, 1366, 2730, 5462, 10922, 21846, 43690, 87382, 174762, 349526, 699050, 1398102};
+static const size_t GROUP_COUNT = sizeof(GROUP_SIZES)/sizeof(GROUP_SIZES[0]);
+
 
 std::vector<int>::iterator lower_bound(std::vector<int>& vec, int start, int end, int target)
 {
@@ -23,34 +26,79 @@ std::vector<int>::iterator lower_bound(std::vector<int>& vec, int start, int end
 
 std::vector<int> create_sorted_list(std::vector<std::vector<int> >& pairs, int straggler, bool hasStraggler)
 {
-	std::vector<int> pend_chain;
-	std::vector<int> main_chain;
+    std::vector<int> pend_chain;
+    std::vector<int> main_chain;
 
-	for (unsigned int i = 0; i < pairs.size(); ++i)
-	{
-		pend_chain.push_back(pairs[i][0]);
-		main_chain.push_back(pairs[i][1]);
-	}
-	main_chain.insert(main_chain.begin(), pend_chain[0]);
+    for (unsigned int i = 0; i < pairs.size(); ++i)
+    {
+        pend_chain.push_back(pairs[i][0]);
+        main_chain.push_back(pairs[i][1]);
+    }
 
-	for (unsigned int index = 1; index < pend_chain.size(); ++index)
-	{
-		std::vector<int>::iterator insertion_point;
-		int item = pend_chain[index];
+    main_chain.insert(main_chain.begin(), pend_chain[0]);
+    std::vector<bool> inserted(pend_chain.size(), false);
+    inserted[0] = true;
 
-		int upper_bound_insert_position = 0;
-		while (pairs[index][1] != main_chain[upper_bound_insert_position])
-			upper_bound_insert_position += 1;
-		insertion_point = lower_bound(main_chain, 0, upper_bound_insert_position, item);
-		main_chain.insert(insertion_point, item);
-	}
-	if (hasStraggler == true)
-	{
-		std::vector<int>::iterator insertion_point = insertion_point;
-		insertion_point = lower_bound(main_chain, 0, main_chain.size(), straggler);
-		main_chain.insert(insertion_point, straggler);
-	}
-	return (main_chain);
+    std::vector<int> uninserted;
+    for (size_t i = 1; i < pend_chain.size(); ++i)
+    {
+        if (!inserted[i])
+        {
+            uninserted.push_back((int)i);
+        }
+    }
+
+    std::vector< std::vector<int> > groups; 
+    {
+        size_t start = 0;
+        size_t remain = uninserted.size();
+        size_t g_idx = 0;
+        while (remain > 0 && g_idx < GROUP_COUNT)
+        {
+            int g_size = GROUP_SIZES[g_idx];
+            if ((size_t)g_size > remain)
+                g_size = (int)remain;
+
+            std::vector<int> group;
+            for (int k = 0; k < g_size; ++k)
+            {
+                group.push_back(uninserted[start + k]);
+            }
+            groups.push_back(group);
+            start += g_size;
+            remain -= g_size;
+            g_idx++;
+        }
+    }
+    for (size_t i = 0; i < groups.size(); ++i)
+    {
+        std::reverse(groups[i].begin(), groups[i].end());
+    }
+
+	size_t upper_bound = 0;
+    for (size_t g = 0; g < groups.size(); ++g)
+    {
+		upper_bound = pow(2, g + 2) - 1;
+        for (size_t el = 0; el < groups[g].size(); ++el)
+        {
+            int idx = groups[g][el];
+            int item = pend_chain[(size_t)idx];
+
+			if (upper_bound > main_chain.size())
+				upper_bound = main_chain.size();
+            std::vector<int>::iterator insertion_point = lower_bound(main_chain, 0, upper_bound, item);
+            main_chain.insert(insertion_point, item);
+            inserted[(size_t)idx] = true;
+        }
+    }
+
+    if (hasStraggler == true)
+    {
+        std::vector<int>::iterator insertion_point = lower_bound(main_chain, 0, (int)main_chain.size(), straggler);
+        main_chain.insert(insertion_point, straggler);
+    }
+
+    return (main_chain);
 }
 
 
@@ -130,18 +178,3 @@ std::vector<int> merge_insertion_sort_vector(std::vector<int> &A)
 		return (create_sorted_list(pairs, straggler, hasStraggler));
 	}
 }
-
-/*
-void debug_print_function(std::string str, std::vector<std::vector<int> > pairs)
-{
-	std::cout << '\n' << str << std::endl;
-	for (std::vector<std::vector<int> >::size_type i = 0; i != pairs.size(); ++i)
-	{
-		for (std::vector<int>::size_type j = 0; j != pairs[i].size(); ++j)
-		{
-			std::cout << pairs[i][j] << ' ';
-		}
-		std::cout << std::endl;
-	}
-}
-*/
--- cpp9/ex02/main.cpp
+++ cpp9/ex02/main.cpp
@@ -1,62 +1,75 @@
 #include "PmergeMe.hpp"
+#include <ctime>     // for clock_t, clock(), CLOCKS_PER_SEC
+#include <cmath>     // for log2, etc.
+#include <vector>
+#include <deque>
+#include <iostream>
+#include <stdexcept> // for std::runtime_error
 
-/*
- ./PmergeMe `shuf -i 1-30 -n 30 | tr "\n" " "`
-*/
+int counter;
 
-int counter = 0;
+static double my_log2(double x)
+{
+    return std::log(x) / std::log(2.0);
+}
+
 
 double worst_case_number_of_comparison(double n)
 {
-    return n * std::ceil(log2(0.75 * n)) - std::floor(pow(2, std::floor(log2(6 * n))) / 3.0) + std::floor(log2(6 * n) / 2.0);	
+    return n * std::ceil(my_log2(0.75 * n))
+           - std::floor(std::pow(2, std::floor(my_log2(6 * n))) / 3.0)
+           + std::floor(my_log2(6 * n) / 2.0);
 }
 
 int main(int argc, char *argv[])
 {
-	try
-	{
-		// Input parsing
-		std::vector<int> Unsorted_vector = input_parsing_vector(argc, argv);
-		unsigned int length_of_vector = Unsorted_vector.size();
+    try
+    {
+        // Input parsing
+        std::vector<int> unsortedVector = input_parsing_vector(argc, argv);
+        unsigned int lengthOfVector = (unsigned int)unsortedVector.size();
 
-		std::deque<int> Unsorted_deque = input_parsing_deque(argc, argv);
-		unsigned int length_of_deque = Unsorted_deque.size();
+        std::deque<int> unsortedDeque = input_parsing_deque(argc, argv);
+        unsigned int lengthOfDeque = (unsigned int)unsortedDeque.size();
 
-		// Print the unsorted list
-		std::cout << "Unsorted list: \n";
-		for (unsigned int i=0; i < length_of_vector; ++i)
-			std::cout << Unsorted_vector[i] << ' ';
+        // Print the unsorted list
+        std::cout << "Unsorted list:\n";
+        for (unsigned int i = 0; i < lengthOfVector; ++i)
+            std::cout << unsortedVector[i] << ' ';
+        std::cout << "\n\n---Performance benchmarking---\n";
 
-		std::cout << '\n' << "\n---Performance benchmarking---" << '\n';
+        // Sort with std::vector
+        clock_t start = clock();
+        std::vector<int> sortedVector = merge_insertion_sort_vector(unsortedVector);
+        clock_t end = clock();
+        double runtime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
 
-		// std::vector
+        std::cout << "\nSorted list (Merge-Insertion) - std::vector:\n";
+        for (unsigned int i = 0; i < lengthOfVector; ++i)
+            std::cout << sortedVector[i] << ' ';
+        std::cout << "\nTime to process " << lengthOfVector << " elements: " << runtime << " ms\n";
+        std::cout << "Number of comparisons: " << counter 
+                  << " (<= worst case bound of " << worst_case_number_of_comparison((double)lengthOfVector) << ")\n";
 
-		clock_t start = clock();
-		std::vector<int>sorted_vector = merge_insertion_sort_vector(Unsorted_vector);
-		clock_t end = clock();
-		double runtime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
-		std::cout << '\n' << "Sorted list - MergeInsertion - std::vector \n";
-		for (unsigned int i=0; i < length_of_vector; ++i)
-			std::cout << sorted_vector[i] << ' ';
-		std::cout << '\n' << "Time to process a range of " << length_of_vector << " elements: " << runtime << " ms" << '\n';
-		std::cout << "Number of comparison for " << length_of_vector << " elements is " << counter << " and is less than or equal as worst case bound of " << worst_case_number_of_comparison(length_of_vector) << " comparison.\n";
+        // Sort with std::deque
+        counter = 0;
+        start = clock();
+        std::deque<int> sortedDeque = merge_insertion_sort_deque(unsortedDeque);
+        end = clock();
+        runtime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
 
-		// std::deque
-		counter = 0;
-		start = clock();
-		std::deque<int>sorted_deque = merge_insertion_sort_deque(Unsorted_deque);
-		end = clock();
-		runtime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
-		std::cout << '\n' << "Sorted list - MergeInsertion - std::deque \n";
-		for (unsigned int i=0; i < length_of_vector; ++i)
-			std::cout << sorted_deque[i] << ' ';
-		std::cout << '\n' << "Time to process a range of " << length_of_deque << " elements: " << runtime << " ms" << '\n';
-		std::cout << "Number of comparison for " << length_of_vector << " elements is " << counter << " and is less than or equal to the worst case bound of " << worst_case_number_of_comparison(length_of_vector) << " comparison.\n";
-	}
-	catch(const std::runtime_error& e)
-	{
-		return (EXIT_FAILURE);
-	}
+        std::cout << "\nSorted list (Merge-Insertion) - std::deque:\n";
+        for (unsigned int i = 0; i < lengthOfDeque; ++i)
+            std::cout << sortedDeque[i] << ' ';
+        std::cout << "\nTime to process " << lengthOfDeque << " elements: " << runtime << " ms\n";
+        std::cout << "Number of comparisons: " << counter
+                  << " (<= worst case bound of " << worst_case_number_of_comparison((double)lengthOfDeque) << ")\n";
+    }
+    catch (const std::runtime_error &e)
+    {
+        std::cerr << "Error: " << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
 
-	return (EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
